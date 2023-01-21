e0d67601c20441a9a9ca0c0eaa6fd448e2128452
joram <jw.kwetters@student.avans.nl>
2023-01-21 21:27:38 +0100

    tutorial #2 content

--- FirstApp/TextBootcamp.swift
+++ FirstApp/TextBootcamp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TextBootcamp: View {
     var body: some View {
-        Text("Hello, World! this is the swiftful thinking bootcam im learn a lot, so this text is also way to big.")
+        Text("Hello, World! this is the swiftful thinking bootcam im learn a lot, so this text is also way to big.".capitalized)
 //            .font(.body)
 //            //.fontWeight(.medium)
 //            .bold()
@@ -18,9 +18,12 @@ struct TextBootcamp: View {
 //            .italic()
 //            .strikethrough(true, color: Color.green)
 //            .font(.system(size: 24, weight: .semibold, design: .serif))
-            .multilineTextAlignment(.center)
-        // test
-        // nog een test
+            //.baselineOffset(10.0)
+            //.kerning(10)
+            .multilineTextAlignment(.leading)
+            .foregroundColor(.red)
+            .frame(width: 200, height: 100, alignment: .center)
+        
     }
 }
 
