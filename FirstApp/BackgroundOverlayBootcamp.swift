06dfe538211e1ea609b2651a1c67e1b4fb6c4d22
joram <jw.kwetters@student.avans.nl>
2023-01-22 16:32:11 +0100

    reformat code of tutorial #9

--- FirstApp/BackgroundOverlayBootcamp.swift
+++ FirstApp/BackgroundOverlayBootcamp.swift
@@ -32,61 +32,61 @@ struct BackgroundOverlayBootcamp: View {
                                     .foregroundColor(Color.white)
                             ).shadow(color: (Color.gray), radius: 10, x: 5, y: 5)
                         ,alignment: .bottomTrailing
+                    )
             )
-                )
         
         
         
         
-//        Rectangle()
-//            .frame(width: 100, height: 100)
-//            .overlay(
-//                Rectangle()
-//                    .fill(Color.blue)
-//                    .frame(width: 50, height: 50),
-//                alignment: .topLeading
-//            )
-//            .background(
-//                Rectangle()
-//                    .fill(Color.red)
-//                    .frame(width: 150, height: 150),
-//                alignment: .bottomTrailing
-//            )
+        //        Rectangle()
+        //            .frame(width: 100, height: 100)
+        //            .overlay(
+        //                Rectangle()
+        //                    .fill(Color.blue)
+        //                    .frame(width: 50, height: 50),
+        //                alignment: .topLeading
+        //            )
+        //            .background(
+        //                Rectangle()
+        //                    .fill(Color.red)
+        //                    .frame(width: 150, height: 150),
+        //                alignment: .bottomTrailing
+        //            )
         
         
         
-//        Circle()
-//            .fill(Color.pink)
-//            .frame(width: 100, height: 100, alignment: .center)
-//            .overlay(
-//            Text("1")
-//                .font(.largeTitle)
-//                .foregroundColor(.white)
-//            )
-//            .background(
-//            Circle()
-//                .fill(Color.purple)
-//                .frame(width: 110, height: 110)
-//            )
+        //        Circle()
+        //            .fill(Color.pink)
+        //            .frame(width: 100, height: 100, alignment: .center)
+        //            .overlay(
+        //            Text("1")
+        //                .font(.largeTitle)
+        //                .foregroundColor(.white)
+        //            )
+        //            .background(
+        //            Circle()
+        //                .fill(Color.purple)
+        //                .frame(width: 110, height: 110)
+        //            )
         
         
         
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//
-//            .background(
-//                //Color.red
-//                //LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
-//                Circle()
-//                    .fill(LinearGradient(gradient: Gradient(colors:[Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
-//                    .frame(width: 100, height: 100, alignment: .center)
-//            )
-//
-//            .background(
-//                Circle()
-//                    .fill(Color.red)
-//                    .frame(width: 120, height: 120, alignment: .center)
-//            )
-                
+        //        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //
+        //            .background(
+        //                //Color.red
+        //                //LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing)
+        //                Circle()
+        //                    .fill(LinearGradient(gradient: Gradient(colors:[Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
+        //                    .frame(width: 100, height: 100, alignment: .center)
+        //            )
+        //
+        //            .background(
+        //                Circle()
+        //                    .fill(Color.red)
+        //                    .frame(width: 120, height: 120, alignment: .center)
+        //            )
+        
     }
 }
 
