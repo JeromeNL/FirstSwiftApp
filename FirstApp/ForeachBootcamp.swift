0f7fa8febb6a396915909044cf4479f74bfc5c99
joram <jw.kwetters@student.avans.nl>
2023-01-23 23:41:10 +0100

    Foreach Tutorial #14

---
+++ FirstApp/ForeachBootcamp.swift
@@ -0,0 +1,41 @@
+//
+//  ForeachBootcamp.swift
+//  FirstApp
+//
+//  Created by Joram Kwetters on 23/01/2023.
+//
+
+import SwiftUI
+
+struct ForeachBootcamp: View {
+    
+    let data: [String] = ["Hi", "Hello", "Hoi"]
+    let myString: String = "Hello"
+    
+    
+    var body: some View {
+        VStack{
+            ForEach(data.indices){ index in
+                Text("\(data[index])")
+            }
+        }
+        
+        
+//        VStack{
+//            ForEach(0..<10){ index in
+//                Text("Hi! \(index + 1)")
+//                HStack {
+//                    Circle()
+//                        .frame(width: 30, height: 30)
+//                    Text("Index: \(index)")
+//                }
+//            }
+//        }
+    }
+}
+
+struct ForeachBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ForeachBootcamp()
+    }
+}
