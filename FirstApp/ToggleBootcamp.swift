3178cc7938f9d2101467f5cd4f77269d5a8781f3
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-27 14:16:28 +0100

    Toggle Tutorial #37

---
+++ FirstApp/ToggleBootcamp.swift
@@ -0,0 +1,40 @@
+//
+//  ToggleBootcamp.swift
+//  FirstApp
+//
+//  Created by Joram Kwetters on 27/01/2023.
+//
+
+import SwiftUI
+
+struct ToggleBootcamp: View {
+    
+    @State var isOn: Bool = false
+    
+    
+    var body: some View {
+        
+        
+        ZStack {
+            var bgColor = isOn ? Color.green : Color.red
+            bgColor.ignoresSafeArea(.all)
+            
+            Toggle(
+                isOn: $isOn,
+                label: {
+                    Text("Label")
+                }
+            )
+        }
+        
+        .toggleStyle(SwitchToggleStyle(tint: Color.red))
+    }
+        
+}
+
+
+struct ToggleBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        ToggleBootcamp()
+    }
+}
