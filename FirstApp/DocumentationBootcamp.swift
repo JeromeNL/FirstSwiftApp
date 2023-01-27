e7fe19dabd3f14e68145e659fef7de0ebba0071f
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-27 23:47:55 +0100

    Documentation Tutorial #45

---
+++ FirstApp/DocumentationBootcamp.swift
@@ -0,0 +1,27 @@
+//
+//  DocumentationBootcamp.swift
+//  FirstApp
+//
+//  Created by Joram Kwetters on 27/01/2023.
+//
+
+// MARK: START OF FILE
+import SwiftUI
+
+struct DocumentationBootcamp: View {
+
+    var body: some View {
+      
+        Text("Hoi!")
+    }
+}
+
+// MARK: just a struct
+
+// This is just a comment
+/// This  is the primary struct
+struct DocumentationBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        DocumentationBootcamp()
+    }
+}
