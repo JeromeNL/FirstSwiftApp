8339ccc71782c8ad6d329d78adcb99a19ef2616a
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-28 18:15:27 +0100

    TextSelection #56

---
+++ FirstApp/TextSelectionBootcamp.swift
@@ -0,0 +1,23 @@
+//
+//  TextSelectionBootcamp.swift
+//  FirstApp
+//
+//  Created by Joram Kwetters on 28/01/2023.
+//
+
+import SwiftUI
+
+struct TextSelectionBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+        
+        
+    }
+}
+
+struct TextSelectionBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        TextSelectionBootcamp()
+    }
+}
