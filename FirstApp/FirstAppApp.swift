44d89fb01a4d1a5e7df254355d2f1e08f755d0e7
JeromeNL <jw.kwetters@student.avans.nl>
2023-01-28 14:54:24 +0100

    @AppStorage Tutorial #52

--- FirstApp/FirstAppApp.swift
+++ FirstApp/FirstAppApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct FirstAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            //ContentView()
+            AppStorageBootcamp()
         }
     }
 }
